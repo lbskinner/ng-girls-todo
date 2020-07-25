c88a14b27386f4ef4446fe5ef654499cdfd8a62b
Leila Skinner <beileiwang@gmail.com>
2020-07-25 14:14:18 -0500

    added static list and learned directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,26 @@ import { Component } from '@angular/core';
     <h1>Welcome to {{ title }}!</h1>
 
     <app-input-button-unit></app-input-button-unit>
+
+    <ul>
+      <!-- *ngFor is an Angular built-in directive
+      works like an enhanced loop in Java
+      * notation causes Angular to use the current element as a templayte when rendering the list -->
+      <li *ngFor="let todoItem of todoList">
+        {{ todoItem.title }}
+      </li>
+    </ul>
   `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-list';
+  todoList = [
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' },
+  ];
 }
