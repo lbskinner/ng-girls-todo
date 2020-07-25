3a493283e3bb94e4e1c2d60dc37263381629ee88
Leila Skinner <beileiwang@gmail.com>
2020-07-25 14:30:50 -0500

    created todo item interface

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
   // tells the component to expect an input and to assign it to the class member called item
-  @Input() item;
+  @Input() item: TodoItem;
   constructor() {}
 
   ngOnInit(): void {}
